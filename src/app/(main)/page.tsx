f4e53b3ca3ac1d2555abc78a4ebba5901860d09a
Chief <49538524+chiemerieokorie@users.noreply.github.com>
2025-07-24 11:26:01 -0400

    Add analytics cards section to dashboard page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,11 @@
 import Dashboard from "@/features/dashboard";
 import {BreadcrumbPage, BreadCrumbs, PageContainer, PageHeader} from "@/components/layout/page-container";
 import {BreadcrumbLink} from "@/components/ui/breadcrumb";
-import {SectionCards} from "@/components/section-cards";
 import {ChartAreaInteractive} from "@/components/chart-area-interactive";
 import {DataTable} from "@/components/data-table";
 import data from "@/app/(main)/data.json";
 import {ROUTES} from "@/lib/constants";
+import {AnalyticsCards} from "@/features/dashboard/analytics-cards";
 
 export default function Home() {
     return (
@@ -18,7 +18,7 @@ export default function Home() {
             </PageHeader>
 
             {/*<Dashboard />*/}
-            <SectionCards/>
+            <AnalyticsCards/>
             <ChartAreaInteractive/>
             <DataTable data={data}/>
         </PageContainer>
