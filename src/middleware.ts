b1dcbda99f2d48eb20090ed3bcfc05bf68e41ffd
Chief <49538524+chiemerieokorie@users.noreply.github.com>
2025-07-25 11:37:37 -0400

    Route by role

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,11 +24,31 @@ export default withAuth(
       return NextResponse.redirect(new URL('/unauthorized', req.url));
     }
 
-    if (pathname.startsWith('/partner') && userRole !== UserRoleEnum.PARTNER_BANK) {
+    if (userRole === UserRoleEnum.ADMIN) {
+        return NextResponse.next();
+    }
+
+    // Merchants page - accessible by ADMIN and PARTNER_BANK
+    if (pathname.startsWith('/merchants') && 
+        ![UserRoleEnum.PARTNER_BANK].includes(userRole as UserRoleEnum)) {
       return NextResponse.redirect(new URL('/unauthorized', req.url));
     }
 
-    if (pathname.startsWith('/merchant') && !['MERCHANT', 'SUB_MERCHANT'].includes(userRole || '')) {
+    // Partner Banks page - accessible by ADMIN only  
+    if (pathname.startsWith('/partner-banks') && userRole !== UserRoleEnum.PARTNER_BANK) {
+      return NextResponse.redirect(new URL('/unauthorized', req.url));
+    }
+
+    // Users page - accessible by ADMIN only
+    if (pathname.startsWith('/users') && ![UserRoleEnum.MERCHANT].includes(userRole || '')) {
+      return NextResponse.redirect(new URL('/unauthorized', req.url));
+    }
+
+    if (pathname.startsWith('/partner') && ![UserRoleEnum.PARTNER_BANK].includes(userRole || '')) {
+      return NextResponse.redirect(new URL('/unauthorized', req.url));
+    }
+
+    if (pathname.startsWith('/merchant') && ![UserRoleEnum.MERCHANT, UserRoleEnum.SUB_MERCHANT].includes(userRole || '')) {
       return NextResponse.redirect(new URL('/unauthorized', req.url));
     }
 
